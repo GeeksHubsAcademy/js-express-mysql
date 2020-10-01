ba147e405af41c3753566a7a38b0d05a69aa153a
DavidPinilla92 <d.pinilla@geekshubs.com>
2020-10-01 10:59:36 +0200

    frontend:add renderProducts method

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,9 +1,25 @@
+const productsDiv = document.querySelector('.products')
+const renderProducts = (products = []) => {
+    productsDiv.innerHTML = '';
+    products.forEach(product => {
+        productsDiv.innerHTML += `
+        <div class="product">
+        <h3>${product.name}</h3>
+        <div>${product.price}</div>
+        <img src="${product.image_path}" alt="${product.name}"/>
+        </div>
+        `
+    })
+}
+
 const getProducts = async() => {
     try {
         let res = await fetch('http://localhost:3000');
         console.log(res);
-        let { products } = await res.json();
-        console.log(products)
+        let {
+            products
+        } = await res.json();
+        renderProducts(products)
     } catch (error) {
 
     }
