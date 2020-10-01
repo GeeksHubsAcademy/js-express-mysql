4ae33f4a26b123a3093d9b4d1a8069f14a16aa52
DavidPinilla92 <d.pinilla@geekshubs.com>
2020-10-01 12:45:19 +0200

    backend:add cors middleware, add put product, add delete product

--- backend/app.js
+++ backend/app.js
@@ -11,10 +11,10 @@ const conn = mysql.createConnection({
 
 app.use(express.json()); //para evitar que el req.body sea undefined
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-
+app.use(function(req, res, next) { //para evitar el error CORS
+    res.header("Access-Control-Allow-Origin", "*"); //permite hacer peticiones desde todos los orígenes
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); //permite peticiones con las cabeceras enumeradas
+    // res.header("Access-Control-Allow-Methods", "GET, POST");
     next();
 });
 
@@ -22,14 +22,17 @@ app.get('/', async(req, res) => {
     try {
         const db = await conn;
         const [products] = await db.execute('SELECT * FROM products');
+        console.log(products)
         res.send({
             products
         });
     } catch (error) {
         console.error(error);
-        res.status(500).send({ message: 'There was a problem trying to get the products' })
+        res.status(500).send({
+            message: 'There was a problem trying to get the products'
+        });
     }
-})
+});
 
 app.post('/', (req, res) => {
     conn.then(db => {
@@ -40,9 +43,40 @@ app.post('/', (req, res) => {
         .then(() => res.status(201).send(req.body))
         .catch(error => {
             console.error(error);
-            res.status(500).send({ message: 'There was a problem trying to create the product' })
+            res.status(500).send({
+                message: 'There was a problem trying to create the product'
+            });
+        });
+
+});
+
+app.put('/:id', async(req, res) => {
+    try {
+        const db = await conn;
+        await db.execute(`
+            UPDATE products 
+            SET name = ?, price = ?, image_path = ?
+            WHERE id = ?`, [req.body.name, req.body.price, req.body.image_path, req.params.id]);
+        res.send({
+            message: 'Product successfully updated'
         })
-
-})
-
-app.listen(PORT, () => console.log('server running on port: ' + PORT))
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({
+            message: 'There was a problem trying to update the product'
+        });
+    }
+});
+app.delete('/:id', (req, res) => {
+    conn.then(db => db.execute(`DELETE FROM products WHERE id = ?`, [req.params.id]))
+        .then(() => res.send({
+            message: 'Product successfully removed'
+        }))
+        .catch(error => {
+            console.error(error);
+            res.status(500).send({
+                message: 'There was a problem trying to remove the product'
+            });
+        })
+});
+app.listen(PORT, () => console.log('server running on port: ' + PORT));
